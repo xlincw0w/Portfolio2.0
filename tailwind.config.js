466917543238feade9a376a51878a2cbf0737476
xlincw0w <khaled.khazem@yahoo.com>
2021-01-01 12:22:11 +0100

    heroku fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: {
-        // enabled: true,
+        enabled: process.env.NODE_ENV === 'production' ? true : false,
         content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     },
     darkMode: false, // or 'media' or 'class'
